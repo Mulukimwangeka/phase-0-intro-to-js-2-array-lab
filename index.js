f3aae3dd5c7f8e46e6d026ae9f2ce5279708b892
mulukimwangeka <mwangekaruth@gmail.com>
2022-08-08 16:43:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(){
+    let newCatOrder = [...cats, "Broom"];
+    return newCatOrder;
+    
+}
+
+function prependCat(){
+    let newCatOrder = ["Arnold", ...cats];
+    return newCatOrder;
+}
+
+function removeLastCat(){
+    return cats.slice(0, -1);
+    
+    
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
